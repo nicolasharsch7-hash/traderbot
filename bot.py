4a8b21f373fa84b0e97a5004053c609f1a390435
nicolasharsch7-hash <nicolasharsch7@gmail.com>
2026-04-29 09:22:27 -0300

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,13 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("8283344299:AAHLeKmYviijZxV1EV4pi-I41QpjqtZE_xs")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot funcionando")
+    print("Recibí /start")
+    await update.message.reply_text("✅ Bot funcionando en Render")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
-print("Bot iniciado...")
-app.run_polling()
+if __name__ == "__main__":
+    print("Bot iniciado correctamente...")
+    app.run_polling(drop_pending_updates=True)
